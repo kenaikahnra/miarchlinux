0a865e1d1bc0199ca57da97849ad5797d9a5494b
kenaikahnra <65419109+kenaikahnra@users.noreply.github.com>
2020-05-25 14:29:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,81 +14,124 @@ lsblk
 echo ""
 read -p "Escribe el nombre del disco donde quieres instalar Arch linux (todo el contenido será borrado): " TARGET
 export TARGET
-read -p "Estas seguro de que deseas borrar todo el contenido de $TARGET? [s/n]: " pBorrado
-if ! [ $pBorrado = 's' ] && ! [ $pBorrado = 'S' ]
-then 
-    echo "Saliendo del instalador."
-    exit
-fi
-wipefs -a /dev/$TARGET &>/dev/null
-read -p "Cuánto espacio (en GiB) quieres dedicar a la partición raiz? para el sistema operativo? " pEspacio
 
-#Información particionado
+#Instalar navegador
+echo "Creación de particiones:"
 echo ""
-echo "A continuación se particionará el disco de la siguiente manera:"
+echo "1.- Utilizar sistema automático de particionado"
+echo "2.- Utilizar las particiones actuales"
 echo ""
-echo "1 - 512Mib - se montará en /boot/efi"
-echo "2 - 2GiB - se utilizará como swap"
-echo "3 - ${pEspacio}Gib - se montará en /"
-echo "4 - el resto del disco se montará en /home"
-echo ""
-read -p 'Continuar? [s/n]: ' fsok
-if ! [ $fsok = 's' ] && ! [ $fsok = 'S' ]
-then 
-    echo "Edita el script para modificar las particiones."
-    exit
-fi
+while :
+do
+    read -p "Qué navegador quieres instalar? " pParticionado
+    echo ""
+    case $pParticionado in
+        1)
+            read -p "Estas seguro de que deseas borrar todo el contenido de $TARGET? [s/n]: " pBorrado
+            if ! [ $pBorrado = 's' ] && ! [ $pBorrado = 'S' ]
+            then 
+                echo "Saliendo del instalador."
+                exit
+            fi
+            wipefs -a /dev/$TARGET &>/dev/null
+            read -p "Cuánto espacio (en GiB) quieres dedicar a la partición raiz? para el sistema operativo? " pEspacio
+            
+            #Información particionado
+            echo ""
+            echo "A continuación se particionará el disco de la siguiente manera:"
+            echo ""
+            echo "1 - 512Mib - se montará en /boot/efi"
+            echo "2 - 2GiB - se utilizará como swap"
+            echo "3 - ${pEspacio}Gib - se montará en /"
+            echo "4 - el resto del disco se montará en /home"
+            echo ""
+            read -p 'Continuar? [s/n]: ' fsok
+            if ! [ $fsok = 's' ] && ! [ $fsok = 'S' ]
+            then 
+                echo "Edita el script para modificar las particiones."
+                exit
+            fi
+            
+            echo ""
 
+            # Crear particiones
+            echo "Creando particiones..."
+            sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/$TARGET
+            o # clear the in memory partition table
+            n # new partition
+            p # primary partition
+            1 # partition number 1
+                # default - start at beginning of disk 
+            +512M # 512 MB boot parttion
+            n # new partition
+            p # primary partition
+            2 # partition number 2
+                # default, start immediately after preceding partition
+            +2G # 8 GB swap partition
+            n # new partition
+            p # primary partition
+            3 # partition number 3
+                # default, start immediately after preceding partition
+            +${pEspacio}G # root partition
+            n # new partition
+            p # primary partition
+            4 # partion number 4
+                # default, start immediately after preceding partition
+                # default, extend partition to end of disk
+            a # make a partition bootable
+            1 # bootable partition is partition 1 -- /dev/sda1
+            p # print the in-memory partition table
+            w # write the partition table
+            q # and we're done
+            EOF
+            echo ""
+            ;;
+        2)
+            break
+            ;;
+        *)
+            echo "Opción no válida, inténtalo de nuevo."
+            ;;
+    esac
+done
 echo ""
 
-# Crear particiones
-echo "Creando particiones..."
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/$TARGET
-  o # clear the in memory partition table
-  n # new partition
-  p # primary partition
-  1 # partition number 1
-    # default - start at beginning of disk 
-  +512M # 512 MB boot parttion
-  n # new partition
-  p # primary partition
-  2 # partition number 2
-    # default, start immediately after preceding partition
-  +2G # 8 GB swap partition
-  n # new partition
-  p # primary partition
-  3 # partition number 3
-    # default, start immediately after preceding partition
-  +${pEspacio}G # root partition
-  n # new partition
-  p # primary partition
-  4 # partion number 4
-    # default, start immediately after preceding partition
-    # default, extend partition to end of disk
-  a # make a partition bootable
-  1 # bootable partition is partition 1 -- /dev/sda1
-  p # print the in-memory partition table
-  w # write the partition table
-  q # and we're done
-EOF
-echo ""
-
-echo "Formateando particiones..."
+echo "Formateando partición de arranque..."
 #Formatear partición /boot
 mkfs.fat -F32 /dev/${TARGET}1
+echo ""
 
 #Formatear partición [swap]
+echo "Formateando partición swap..."
 mkswap /dev/${TARGET}2
 swapon /dev/${TARGET}2
+echo ""
 
-#Formatear partición / y /home
+#Formatear partición /
+echo "Formateando partición raiz..."
 mkfs.ext4 -F /dev/${TARGET}3
-mkfs.ext4 -F /dev/${TARGET}4
+echo ""
+
+#Formatear /home
+
+if ! [ $pParticionado = '2' ]
+then
+    read -p 'Deseas formatear la partición /home? [s/n]: ' pHome
+    if ! [ $pHome = 's' ] && ! [ $pHome = 'S' ]
+        then 
+        echo "Formateando partición /home..."
+        mkfs.ext4 -F /dev/${TARGET}4
+    fi
+else
+    echo "Formateando partición /home..."
+    mkfs.ext4 -F /dev/${TARGET}4
+fi
+echo ""
 
 #Montar partición /
 mount /dev/${TARGET}3 /mnt
 
-#Montar partición /home si la hubiera
+#Montar partición /home
 mkdir /mnt/home
 mount /dev/${TARGET}4 /mnt/home
 
