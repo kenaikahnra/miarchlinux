20dbbe096383f4304926c450256d60b320207d80
kenaikahnra <65419109+kenaikahnra@users.noreply.github.com>
2020-06-19 19:23:57 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -210,12 +210,21 @@ then
 fi
 echo ""
 
+#Instalar Nvidia
+read -p "Quieres instalar los drivers de Nvidia? [s/n] " pNvidia
+if [ $pNvidia = 's' ] || [ $pNvidia = 'S' ]
+then
+    echo "Instalando drivers de Nvidia"
+    pacman -S --noconfirm --needed nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
+fi
+echo ""
+
 #Instalar Wine
 read -p "Quieres instalar Wine? [s/n] " pWine
 if [ $pWine = 's' ] || [ $pWine = 'S' ]
 then
     echo "Instalando Wine, Dxvk y sus dependencias"
-    pacman -S --noconfirm --needed wine-staging winetricks nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
+    pacman -S --noconfirm --needed wine-staging winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
 fi
 echo ""
 
